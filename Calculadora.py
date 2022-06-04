07ddb499e369a3dba0a2bf4a8375df5407e579f6
DavidJAlonso <david.pozo@asesormasmovil.es>
2022-06-04 20:16:24 +0200

    Raiz Cuadrada

--- Calculadora.py
+++ Calculadora.py
@@ -6,8 +6,20 @@ class Calculadora:
         return a - b
     def multiplicacion(self, a, b):
         return a * b    
-     def division(self, a, b):
+    def division(self, a, b):
         if b == 0:
             return "error"
         else:
-            return a / b   
+            return a / b
+    def sqrt(self, n):
+        x = n /2
+
+        while True:
+            if x * x == n:
+                return x
+            else:
+                copia_x = x
+                x = (x + (n/x)) / 2
+            if copia_x == x:
+                break
+        return x   
